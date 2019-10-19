9997e79214f5e675c2b0432958787b0ed93befd1
sAnjali12 <anjalisen060@gmail.com>
2019-10-19 22:35:46 +0530

    create route for post products

--- productBill.js
+++ productBill.js
@@ -4,80 +4,31 @@ var mysql = require('mysql');
 app.use(express.json())
 var knex = require('./conection.js');
 
-// console.log('database is connected now!');
+function getTax(price,tax){
+    return price*tax/100
+}
 
-
-app.post('/product',(req,res)=>{
-    var stages = ["book","medicine","food"]
-    var obj={Product:req.body.Product,price:req.body.price,Country:req.body.Country,stage:req.body.stage}
-    if(stages.hasOwnProperty((obj.stage))&&("India").hasOwnProperty(obj.Country)){
-        obj.SalesTaxes=0.00;
-        Total=obj.price
-        obj.totalPrice=Total
-
-    // }else if(["book","medicine","food"].includes(obj.stage) && (["OtherCountry"].includes(obj.Country))){
-    //     obj.SalesTaxes=5.00;
-    //     tax=obj.price*obj.SalesTaxes/100;
-    //     obj.SalesTaxes=tax
-    //     obj.totalPrice=tax+obj.price;
-
-    }if(["general"].includes(obj.stage)&&("India").includes(obj.Country)){
-        obj.SalesTaxes=10.00;
-        tax=obj.price*obj.SalesTaxes/100;
-        obj.SalesTaxes=tax
-        obj.totalPrice=tax+obj.price;
-
-    }else{
-        obj.SalesTaxes=15.00;
-        tax=obj.price*obj.SalesTaxes/100;
-        obj.SalesTaxes=tax
-        obj.totalPrice=tax+obj.price;
+app.post("/add-products",function(req,res){
+    categories=["book","food","medicine"]
+    productDetails={"Product":req.body.Product,"imported":req.body.imported,"category":req.body.category,"Price":req.body.Price,"Tax":req.body.Tax,"TotalPrice":req.body.TotalPrice}
+    var salseTax=0;
+    if(productDetails.imported){
+        salseTax+=getTax(productDetails["Price"],5);
     }
-    knex.select('*').from('SalesTax').insert(obj)
+    if(!categories.includes(productDetails.category)){
+        salseTax+=getTax(productDetails["Price"],10);
+    }
+   
+    productDetails["Tax"]=salseTax
+    productDetails["TotalPrice"]=productDetails["Price"]+salseTax
+
+    knex('productsBill').insert(productDetails)
     .then((result)=>{
-        return res.send("Iserted data!!!!...")
+        return res.json({ success: true, message: 'ok' });
     }).catch((err)=>{
-        return res.send(err);
-    })
+        res.send(err)
+    });
 })
-
-app.get("/get_product",function(req,res){
-    knex.select("Product"," price"," SalesTaxes"," totalPrice").from("SalesTax").then((result)=>{
-        tax_list=[], price_list=[], total_list=[], pro_list=[]
-
-        for (index of result){
-            pro_list.push(index["Product"])
-            tax_list.push(index[" SalesTaxes"])
-            price_list.push(index["price"])
-            total_list.push(index["totalPrice"])
-        }
-        count=0
-        tax_total = 0
-        list=[]
-        grand_obj={}
-        for (var i=0; (i<tax_list.length); i++){
-            data={}
-            data.product=pro_list[i]
-            data.price=price_list[i]
-            data.tax=tax_list[i]
-            data.total_price=total_list[i]
-            tax = total_list[i] - price_list[i]
-            console.log(tax)
-            tax_total = tax + tax_total
-            console.log(tax_total)
-            count=count+total_list[i]
-            list.push(data)
-        }
-        grand_obj.grand_total=count
-        grand_obj.tax_total = tax_total
-        list.push(grand_obj)
-        res.send(list)
-    
-    }).catch((err)=>{
-        res.send("their is something wrong")
-    })
-})
-
 app.listen(8001,() =>{
     console.log("listining 8001.... ");
 });
