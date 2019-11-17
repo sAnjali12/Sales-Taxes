f2a7975eca826233634ca268e5be161a6c417269
sAnjali12 <anjalisen060@gmail.com>
2019-11-17 10:29:24 +0530

    insrt the data in cart table by ccartProducts.js file

--- Routes/cartProducts.js
+++ Routes/cartProducts.js
@@ -1,26 +1,90 @@
 const express = require('express');
+var cartDb=require("../Modle/cartproducts");
+var products = require("./calculate.js")
 var cart = express.Router();
 cart.use(express.json())
-const add = require("../Modle/product");
+
+
+function ProDetail(product){
+    var tax=0;
+    var price=0;
+    var totalPro={};
+    var productWithtax=[];
+    for (var i=0;  i<product.length; i++){
+
+        var productDetails=products(product[i])
+        tax=tax+productDetails["tax"]
+        price=price+productDetails["quantityPrice"]
+
+        productWithtax.push(productDetails)
+    }totalPro["totalBill"]=price
+    totalPro["totalTax"]=tax
+    productWithtax.push(totalPro);
+    return productWithtax;
+
+}  
 
 
 cart.get("/:name/:quantity",function(req,res){
-    productName = req.params.name;
-    productQuantity = req.params.quantity;
-    let resp  = add.selectData(productName).then((data)=>{
-        return res.json(data)
+    var productName = req.params.name;
+    var productQuantity = req.params.quantity;
+    var id=req.body.No;
+    var products=cartDb.selectData(productName)
+    products.then((data)=>{
+    inserting=cartDb.insertion(data[0],productQuantity,id)
+    return inserting
+    }).then((result)=>{
+        res.json("data inserted")
+    })
+    .catch((err)=>{
+        res.send(err)
+    });
+
+});
+
+
+cart.get("/produts",(req,res)=>{
+
+    var cartProduts=cartDb.getData()
+    cartProduts.then((productsD)=>{
+    data = ProDetail(productsD)
+    res.json(data)
     }).catch((err)=>{
-        return(err);
+        console.log(err)
+        res.send(err)
     })
 });
-    
-
-    // let postData = add.selectData(productName,productQuantity)
-    // console.log('DATA POST.....')
 
 
+cart.get("/:id",(req,res)=>{
+    var id  = req.params.id
+    var data = cartDb.selectproduct(id)
+   data.then((Response)=>{
+       res.json(Response)
+   }).catch((err)=>{
+       console.log(err)
+       res.send(err)
+   })
+});
 
 
 
 
-module.exports = cart
+module.exports = cart;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
